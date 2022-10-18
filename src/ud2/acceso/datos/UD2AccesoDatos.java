6f7f2221ba0cf8311ee11eadbbb373bcb354c804
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-18 11:43:05 +0200

    Commit: creacion de tema 2 de acceso a datos

---
+++ src/ud2/acceso/datos/UD2AccesoDatos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ud2.acceso.datos;
+
+/**
+ *
+ * @author IFC
+ */
+public class UD2AccesoDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
